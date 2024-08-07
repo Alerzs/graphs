2aef8aa2ba431b85cc632b94bc4d2ad6dea0c762
Alireza Shams <alerzs1023@gmail.com>
2024-08-07 23:24:56 +0330

    template

---
+++ template.py
@@ -0,0 +1,37 @@
+import json
+import csv
+
+class Graph:
+
+
+    def __init__(self ,file) -> None:
+        
+        pass
+        
+
+    def yaal_count(self):
+     pass
+
+    def ras_count(self):
+        pass
+    
+
+
+    def BFS(self, org, des):
+        if self.type == "weighted":
+            print("BFS is not allowed for weighted graphs")
+            return
+
+    def DFS(self):
+        pass
+
+    def djkstra(self):
+        pass
+
+    
+class Yaal:
+    
+    def __init__(self ,org ,des ,weight) -> None:
+        self.org = org
+        self.des = des
+        self.weight = weight
